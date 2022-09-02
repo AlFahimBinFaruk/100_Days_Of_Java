e19cdccb539c854ac9ff0d46742a0ddf841263e2
al fahim bin faruk <fmd03700@gmail.com>
2022-09-03 00:41:23 +0600

    day 01

---
+++ src/pkg100daysofjava/EascapeSequence.java
@@ -0,0 +1,19 @@
+package pkg100daysofjava;
+
+public class EascapeSequence {
+
+    public static void main(String[] args) {
+//        back space
+        System.out.println("1. Fir\bst Java Programme.");
+//        new line 
+        System.out.println("2. Hello \n Java.");
+//        form feed
+        System.out.println("3. Hello \f Fahim.");
+//        carriage return
+        System.out.println("Hello \r 4. Suhan.");
+//      tab
+        System.out.println("5. Hello \t Al Fahim.");
+//      print " , ' , \
+        System.out.println("6. \" Al Fahim \' Suhan \'  \" ");
+    }
+}
---
+++ src/pkg100daysofjava/FirstProgrammer.java
@@ -0,0 +1,9 @@
+package pkg100daysofjava;
+
+public class FirstProgrammer {
+
+    public static void main(String[] args) {
+        System.out.println("First Java Programme.");
+        System.out.print("Hello Java.");
+    }
+}
